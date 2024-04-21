95275084c294e9460e47f9fd1a56ef1fb5f3ef76
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2024-04-21 15:18:24 +0100

    Updated file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-#!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package {'flask':
+package { 'Werkzeug' :
+  ensure   => '2.1.1',
+  name     => 'werkzeug',
+  provider => 'pip3'
+}
+
+package { 'flask' :
   ensure   => '2.1.0',
+  name     => 'flask',
   provider => 'pip3'
 }
