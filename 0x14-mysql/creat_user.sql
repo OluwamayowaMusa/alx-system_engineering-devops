628518a68a981bfe279a53cf6f40fe49b91e9e99
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2024-05-21 10:30:07 +0100

    Updated file

---
+++ 0x14-mysql/creat_user.sql
@@ -0,0 +1,4 @@
+-- CREATE USER
+-- GRANT PERMISSION
+CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
