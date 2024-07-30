de6e1a56435a9eabd763b2c3f8daae043ee1466c
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2024-07-30 08:06:12 +0100

    Updated Sql files

--- 0x14-mysql/create_database.sql
+++ 0x14-mysql/create_database.sql
@@ -1,5 +1,6 @@
 -- CREATE DATABASE
 -- CREATE TABLE
+-- GRANT PERMISSION
 CREATE DATABASE IF NOT EXISTS tyrell_corp;
 USE tyrell_corp;
 CREATE TABLE nexus6
@@ -7,3 +8,4 @@ CREATE TABLE nexus6
    name VARCHAR(255));
 INSERT INTO nexus6 (id, name) VALUES
   (1, "Leon");
+GRANT SELECT ON tyrell_corp.nexus6 TO 'holberton_user'@'localhost';
