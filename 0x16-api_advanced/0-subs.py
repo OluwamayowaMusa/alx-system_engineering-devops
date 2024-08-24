d5f454f877b222ae33b1c8368878ce8378cf7702
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2024-08-25 00:36:20 +0100

    Refactored to support python 3.4

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,7 +16,9 @@ def number_of_subscribers(subreddit):
     request_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)\
                              AppleWebKit/537.36 (KHTML, like Gecko)\
-                             Chrome/50.0.2661.102 Safari/537.36"}
+                             Chrome/50.0.2661.102 Safari/537.36",
+               'referer': 'https://...'
+               }
     response = requests.get(request_url,
                             headers=headers,
                             allow_redirects=False)
