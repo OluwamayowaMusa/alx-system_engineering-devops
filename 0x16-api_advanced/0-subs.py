681133c59e70cd026c83fb95008ef5b65c326997
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2024-08-24 19:38:16 +0100

    Task 1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Queries the reddit API """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+        Get the number of subscribers for a given subreddit
+
+
+    Args:
+        subreddit(str): Given subreddit
+
+    Returns: Number of subscribers
+    """
+    request_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(request_url)
+
+    if response:
+        response = response.json()
+        return response.get("data").get("subscribers")
+
+    return 0
