4be5cb8749ad5086cc2553f2a48c7cdcd4e517d9
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2024-08-24 19:53:51 +0100

    Task 1 refactored

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
-
+"""Queries the reddit API """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    """
+        Get the number of subscribers for a given subreddit
+
+
+    Args:
+        subreddit(str): Given subreddit
+
+    Returns: Number of subscribers
+    """
+    request_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(request_url).json()
+
+    return response.get("data", {}).get("subscribers", 0)
